91c5bbd47dccb6dceba7099ae99cdb2f6ac01dbc
Jatin Kumar <main.jatink@gmail.com>
2023-03-10 01:23:00 +0530

    update(connection): fix connection and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,129 @@
 import React, { useState, useEffect } from "react";
 import Navigation from "./navigation";
 import Cookies from "js-cookie";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 
 import { useDispatch } from "react-redux";
-import { bindActionCreators } from 'redux'
-import { actionCreators } from './state';
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state";
 
 import axios from "axios";
 import { API_URI } from "./constants/api.url";
 
 const App = () => {
-  const state = useSelector(s => s)
+  const state = useSelector((s) => s);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [onBoarding, setIsOnBoarding] = useState(true);
 
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const { setUser } = bindActionCreators(actionCreators, dispatch);
 
   useEffect(() => {
-    let uid = Cookies.get('uid');
+    let uid = Cookies.get("uid");
 
     if (uid != null) {
-      axios.get(`${API_URI}/users/club/single/${uid}`)
-        .then(res => {
-          console.log("user data", res.data)
-          setUser(res.data)
-        }).catch(err => {
-          console.log(err)
-        })
-    }
-
-  }, [])
-  useEffect(() => {
-    let uid = Cookies.get('uid');
-
-    if (uid != null) {
-      axios.get(`${API_URI}/users/student/single/${uid}`)
-        .then(res => {
-          console.log("user data", res.data)
-          setUser(res.data)
-        }).catch(err => {
-          console.log(err)
-        })
-    }
-
-  }, [])
-  useEffect(() => {
-    let uid = Cookies.get('uid');
-
-    if (uid != null) {
-      axios.get(`${API_URI}/users/faculty/single/${uid}`)
-        .then(res => {
-          console.log("user data", res.data)
-          setUser(res.data)
-        }).catch(err => {
-          console.log(err)
-        })
-    }
-
-  }, [])
-
-  useEffect(() => {
-    console.log("Redux State", state)
-    if (state.user != null) {
-      setIsLoggedIn(true)
-      const status = state.user.isComplete;
-      console.log("status", status)
-      if (status == false) {
-        setIsOnBoarding(true)
+      if (Cookies.get("user") === "club") {
+        axios
+          .get(`${API_URI}/users/club/single/${uid}`)
+          .then((res) => {
+            console.log("user data", res.data);
+            setUser(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else if (Cookies.get("user") === "student") {
+        axios
+          .get(`${API_URI}/users/student/single/${uid}`)
+          .then((res) => {
+            console.log("user data", res.data);
+            setUser(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       } else {
-        setIsOnBoarding(false)
+        axios
+          .get(`${API_URI}/users/faculty/single/${uid}`)
+          .then((res) => {
+            console.log("user data", res.data);
+            setUser(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       }
     }
-    else {
-      setIsLoggedIn(false)
-      setIsOnBoarding(false)
+  }, []);
+  // useEffect(() => {
+  //   let uid = Cookies.get('uid');
+
+  //   if (uid != null) {
+  //     axios.get(`${API_URI}/users/student/single/${uid}`)
+  //       .then(res => {
+  //         console.log("user data", res.data)
+  //         setUser(res.data)
+  //       }).catch(err => {
+  //         console.log(err)
+  //       })
+  //   }
+
+  // }, [])
+  // useEffect(() => {
+  //   let uid = Cookies.get('uid');
+
+  //   if (uid != null) {
+  //     axios.get(`${API_URI}/users/faculty/single/${uid}`)
+  //       .then(res => {
+  //         console.log("user data", res.data)
+  //         setUser(res.data)
+  //       }).catch(err => {
+  //         console.log(err)
+  //       })
+  //   }
+
+  // }, [])
+
+  useEffect(() => {
+    console.log("Redux State", state);
+    if (state.user != null) {
+      setIsLoggedIn(true);
+      const status = state.user.isComplete;
+      console.log("status", status);
+      if (status == false) {
+        setIsOnBoarding(true);
+      } else {
+        setIsOnBoarding(false);
+      }
+    } else {
+      setIsLoggedIn(false);
+      setIsOnBoarding(false);
     }
-  }, [state])
+    console.log(state)
+  }, [state]);
 
   return (
     <>
-      <Toaster position="top-right"
+      <Toaster
+        position="top-right"
         toastOptions={{
           style: {
             width: "20vw",
-            border: '1px solid #000000',
-            padding: '16px',
-            color: '#000000',
+            border: "1px solid #000000",
+            padding: "16px",
+            color: "#000000",
             duration: 10000,
           },
-        }} />
-      <Navigation isLoggedIn={isLoggedIn} onBoarding={onBoarding} />
+        }}
+      />
+      <Navigation
+        user={state.user}
+        isLoggedIn={isLoggedIn}
+        onBoarding={onBoarding}
+      />
     </>
   );
 };
--- src/constants/route.jsx
+++ src/constants/route.jsx
@@ -1,12 +1,32 @@
+import Cookies from "js-cookie";
+import ClubStudentsComp from "../components/club/clubstudents.comp";
+import FacultyComp from "../components/faculty/dashboard.comp";
+import FacultyStudentsComp from "../components/faculty/fstudents.comp";
+import FacultyProjectsComp from "../components/faculty/projects.comp";
+import FacultyResearchComp from "../components/faculty/research.comp";
 import HomeComp from "../components/home/home.comp";
-import Onboarding from "../components/onboarding/onboarding";
+import Onboarding from "../components/onboarding/studentOnboarding";
+import StudentComp from "../components/student/student.comp";
 import LoginPage from "../pages/login.page";
 import SignupPage from "../pages/signup.Page";
+const userType = Cookies.get("user")
 
 export const PRIVATE_ROUTES = [
   {
-    path: "/",
-    component: <HomeComp />
+    path: "/dashboard",
+    component: userType == "faculty" ? <FacultyComp /> : userType == "student" ? <StudentComp /> : <ClubStudentsComp />,
+  },
+  {
+    path: "/facStud",
+    component: <FacultyStudentsComp />
+  },
+  {
+    path: "/facproj",
+    component: <FacultyProjectsComp />
+  },
+  {
+    path: "/facppr",
+    component: <FacultyResearchComp />
   }
 ];
 export const PUBLIC_ROUTES = [
